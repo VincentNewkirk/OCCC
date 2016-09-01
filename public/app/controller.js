065f1b3cdb3c296a2a0a181fede5fe31d8f70c48
Tyler Chong <tylrchong@gmail.com>
2016-08-31 22:28:23 -1000

    Bcrypt Encryption set up

--- public/app/controller.js
+++ public/app/controller.js
@@ -4,7 +4,7 @@ angular.module('formSignupApp', [])
     $scope.password = '';
     $scope.update = (user) => {
       const req = new XMLHttpRequest();
-      req.open('POST', 'http://localhost:4002/testFBEmailSignup');
+      req.open('POST', 'http://localhost:4002/signup');
       req.setRequestHeader("Authorization", "Negotiate");
       req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
       req.send(JSON.stringify(user));
