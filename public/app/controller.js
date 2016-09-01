2518f20a8ecba348902ac06497c3e0839e4ffa5d
dpcc <drewpcchannel@gmail.com>
2016-08-31 14:11:53 -1000

    got a static object to send to server

--- public/app/controller.js
+++ public/app/controller.js
@@ -3,12 +3,23 @@ angular.module('formSignupApp', [])
     $scope.email = '';
     $scope.password = '';
     $scope.update = function(user) {
+
+      var req = new XMLHttpRequest();
+      req.open('POST', 'http://localhost:4002/testFBEmailSignup');
+      req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      var obj = {hello:'world'};
+      req.send(JSON.stringify(obj));
+
+    };
+/*    $scope.update = function(user) {
       var request = new XMLHttpRequest();
       request.open('POST', 'http://localhost:4002/testFBEmailSignup', true);
-      request.send(user);
+      xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      console.log(JSON.stringify(user));
+      request.send(JSON.stringify(user));
 
       if (request.status === "200") {
         console.log(request.responseText);
       }
-    };
+    };*/
   }]);
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const app = express();
 const firebase = require('firebase');
+const bodyParser = require('body-parser')
 
 //not sure if this will work with out exporting config
 //might need to auth any domain using firebase in the console - auth
 //form needs homepage redirect, or login page
 
-var config = {
+const config = {
   apiKey: "AIzaSyDr-cAxhiDSQqlQfe5jGc-5UsQ0l6La5FE",
   authDomain: "calendar-test-950b1.firebaseapp.com",
   databaseURL: "https://calendar-test-950b1.firebaseio.com",
   storageBucket: "calendar-test-950b1.appspot.com",
 };
-
 firebase.initializeApp(config);
+
+/* MIDDLEWARE */
+app.use(bodyParser.json());
 
 function emailSignup(email, password) {
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    let errorCode = error.code;
+    let errorMessage = error.message;
   });
 }
 
 app.post('/testFBEmailSignup', (req, res) => {
-  console.log(res)
+  console.log(req.body)
   // emailSignup('test@something.com', 'idk123');
 })
 
