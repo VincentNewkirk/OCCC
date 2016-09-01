70454a1fdb4f4a899766cd067e5fba98718660d0
Tyler Chong <tylrchong@gmail.com>
2016-08-31 16:54:22 -1000

    Prepping to merge in Upstream/master

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const firebase = require('firebase');
 //might need to auth any domain using firebase in the console - auth
 //form needs homepage redirect, or login page
 
-var config = {
+const config = {
   apiKey: "AIzaSyDr-cAxhiDSQqlQfe5jGc-5UsQ0l6La5FE",
   authDomain: "calendar-test-950b1.firebaseapp.com",
   databaseURL: "https://calendar-test-950b1.firebaseio.com",
@@ -23,12 +23,12 @@ function emailSignup(email, password) {
 }
 
 app.post('/testFBEmailSignup', (req, res) => {
-  console.log(res)
+  console.log(res);
   // emailSignup('test@something.com', 'idk123');
-})
+});
 
 app.use(express.static('public'));
 
 app.listen(4002, (req, res) => {
   console.log('Server listening on 4002');
-})
+});
