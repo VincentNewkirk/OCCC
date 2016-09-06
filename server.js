48c2bbde827fc6f16380d74022fb85165f53f18c
Tyler Chong <tylrchong@gmail.com>
2016-09-05 15:08:04 -1000

    Merged in latest changes

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 //might need to auth any domain using firebase in the console - auth
 //form needs homepage redirect, or login page
 //make forms use ng-repeat
+//pages load angular each time they load...
+//adobe sign free trial
+//firebase has simple UI for logins
 
 const config = {
   apiKey: "AIzaSyDr-cAxhiDSQqlQfe5jGc-5UsQ0l6La5FE",
@@ -59,7 +62,7 @@ const validateEmail = email => {
 app.post('/login', (req, res) => {
   firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
   .catch((error) => {
-    console.log(`${error.code} : ${error.message}`);
+    console.log(`this is the firebase error code from login- ${error.code} : ${error.message}`);
   });
 });
 
@@ -69,6 +72,12 @@ app.get('/signout', (req, res) => {
   }, (error) => {
     console.log(`Error with signout ${error}`);
   });
+});
+
+app.get('/testPage', (req, res) => {
+  console.log('attempting to find current user.. ')
+  var user = firebase.auth().currentUser;
+  console.log(user)
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
